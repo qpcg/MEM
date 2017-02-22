fed26c959f44a0a5c0761522c2113475e14a7a5e
Alex <alex@qpcg.io>
2017-02-22 14:57:32 -0500

    Added the rest of the functions

---
+++ Gauher.R
@@ -0,0 +1,11 @@
+#' Gauher
+#'
+#' Simply gets the nth item from a list. Not even sure why this is a function.
+#' @param n
+#' #@export
+#' #@examples
+#' Gauher()
+
+Gauher <-function (n) {
+  gh[[n]]
+}
